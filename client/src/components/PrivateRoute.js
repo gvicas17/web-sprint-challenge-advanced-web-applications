6a2f54f80ad23c44ddd764648dc68d9d75ab6b16
Gabby Vicas <gabbyvicas@Gabbys-MacBook-Pro.local>
2020-11-13 13:10:47 -0600

    created private route

---
+++ client/src/components/PrivateRoute.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import {Redirect, Route} from 'react-router-dom'
+
+export const PrivateRoute = ({component: Component, ...rest}) => {
+    const token = window.localStorage.getItem('token')
+    return(
+        <Route {...rest} render = {(props) => {
+            if(token){
+                return<Component {...props}/>
+            }
+            else{
+                return <Redirect to = '/'/>
+            }
+        }}/>
+    )
+}
